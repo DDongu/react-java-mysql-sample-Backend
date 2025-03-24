f8b43220aeb8c722c79b647921edecc9f5e69b37
김동규 <dongu@MacBook-Pro.local>
2025-03-24 16:33:27 +0900

    Edit: cicd 방식 변경

---
+++ deployment/scripts/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 기존 배포 폴더 삭제
+rm -rf /home/ec2-user/backend
+
+# 새 폴더 생성
+mkdir -p /home/ec2-user/backend
--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 중지된 컨테이너 제거
-docker stop backend-container || true
-docker rm backend-container || true
+# 최신 MONGO_URI 가져오기
+MONGO_URI=$(aws secretsmanager get-secret-value --secret-id DocumentDBSecret --query 'SecretString' --output text | jq -r .MONGO_URI)
 
-# 최신 Docker 이미지 pull
+# 기존 컨테이너 중지 및 삭제
+docker stop my-backend-app || true
+docker rm my-backend-app || true
+
+# 기존 Docker 이미지 삭제 (디스크 공간 절약)
+docker rmi $(docker images -q) || true
+
+# 최신 Docker 이미지 가져오기 (latest 태그 유지)
 docker pull $DOCKER_USERNAME/my-backend-app:latest
 
-# 새로운 컨테이너 실행
-docker run -d --name backend-container -p 8080:8080 \
-  -e SPRING_DATASOURCE_URL=jdbc:mysql://database-host:3306/coronatracker \
-  -e SPRING_DATASOURCE_USERNAME=dbuser \
-  -e SPRING_DATASOURCE_PASSWORD=dbpassword \
-  $DOCKER_USERNAME/my-backend-app:latest
+# 컨테이너 실행 (DocumentDB 연결)
+docker run -d --name my-backend-app -p 8080:8080 -e MONGO_URI=$MONGO_URI $DOCKER_USERNAME/my-backend-app:latest
