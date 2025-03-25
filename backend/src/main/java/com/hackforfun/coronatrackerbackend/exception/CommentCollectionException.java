14397864ed46ce52f55cb39506a92d6c93a4a238
김동규 <dongu@MacBook-Pro.local>
2025-03-26 00:15:07 +0900

    .

--- backend/src/main/java/com/hackforfun/coronatrackerbackend/exception/CommentCollectionException.java
+++ backend/src/main/java/com/hackforfun/coronatrackerbackend/exception/CommentCollectionException.java
@@ -1,36 +1,20 @@
-package com.hackforfun.coronatrackerbackend.controller;
+package com.hackforfun.coronatrackerbackend.exception;
 
-import com.hackforfun.coronatrackerbackend.model.Comment;
-import com.hackforfun.coronatrackerbackend.service.CommentService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@CrossOrigin(origins = "*")
-public class CommentController {
-    private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
-
-    @Autowired
-    private CommentService commentService;
-
-    @GetMapping("/getComments")
-    public ResponseEntity<?> getAllComments() {
-        try {
-            List<Comment> comments = commentService.getAllComments();
-            return ResponseEntity.ok(comments);
-        } catch (Exception e) {
-            logger.error("댓글 조회 중 오류 발생", e);
-            return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("댓글을 불러오는 중 오류가 발생했습니다: " + e.getMessage());
-        }
+public class CommentCollectionException extends Exception {
+	
+	public CommentCollectionException(String message)
+    {
+        super(message);
     }
 
-    // 다른 메서드들도 유사하게 예외 처리
+    public static String NotFoundException(String id)
+    {
+        return "Comment with "+id+" not found";
+    }
+
+    public static String TitleAlreadyExists()
+    {
+        return "Comment with given title already exists";
+    }
+
 }
